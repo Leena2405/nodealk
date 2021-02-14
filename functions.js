52bf15ed23286bccd0122e8eea9e8b2d5e1951db
[Leena2405] <[zaneta.lenart@gmail.com]>
2021-02-14 13:06:45 +0100

    first

---
+++ functions.js
@@ -0,0 +1,13 @@
+const helloWorld = () => console.log("New hello World");
+const add = (a,b) => console.log(a+b)
+const subtitle = "zajecia backed"
+const sample = () =>'proba funkcji';
+
+module.exports ={
+    helloWorld,
+    add,
+    subtitle,
+    sample
+}
+
+console.log(" functions.js loaded correctly");
